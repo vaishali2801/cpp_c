b5fe33a0ffa38ef0f4c4be4d41aa07e77b878286
vaishali <vaishalichauhan2801@gmail.com>
2025-06-13 13:09:18 +0530

    1&2

---
+++ 01_1_C++_INTRO/index.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int a;
+   int b;
+    cin >> a;
+    cin >> b;
+
+   cout << "a+b is :" << a+b << endl ;
+   cout << "a-b is :" << a-b << endl ;
+   cout << "a*b is :" << a*b << endl ;
+   cout << "a/b is :" << a/b << endl ;
+   cout << "a%b is :" << a%b << endl ;
+
+   float pi= 3.14;
+
+   cout << pi << endl;
+
+   char star ='*';
+
+   cout << star << endl;
+
+   string name = "vaishali";
+
+   cout << name << endl;
+
+   bool age = 18;
+
+   cout << age << endl;
+    return 0;
+}
---
+++ 02_2_C++_OPERATOR/index.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    // int a = 25;
+    // int b = 35;
+    
+    // int x = 100;
+    // int y = 100;
+     
+    // cout << (a>b) << endl;
+    // cout << (a<b) << endl;
+
+    // cout << (a==b && x==y) << endl;
+    // cout << (a==b || x==y) << endl;
+    
+    // int l = 200;
+    // char p = 'V';
+
+    // cout << sizeof(l) << endl;
+    // cout << sizeof(p) << endl;
+    
+    // // cout << ++x << endl;
+    // // cout << ++b << endl;
+    
+    // cout << --x << endl;
+    // cout << --b << endl;
+
+    // cout << x++ << endl;
+    // cout << b++ << endl;
+   
+
+    float w = 9.9;
+    int r = 3.3;
+
+    cout << (int)w + r << endl;
+
+
+     return 0;
+ }
